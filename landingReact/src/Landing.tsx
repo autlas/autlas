1d3492afcb40e35c2a42c7a56cd97c7296b315cb
heavymax <maximlegkii@gmail.com>
2026-04-19 01:11:18 +0300

    tweak(landing): github logo on both star buttons, drop v0.1.0 badge

--- landingReact/src/Landing.tsx
+++ landingReact/src/Landing.tsx
@@ -129,7 +129,6 @@ export default function Landing() {
         <a href="#" className="nav-logo">
           <img src="/assets/logo.png" alt="" className="brand" />
           autlas
-          <span className="ver">v0.1.0</span>
         </a>
         <ul>
           <li><a href="#problem">Why</a></li>
@@ -174,7 +173,7 @@ export default function Landing() {
               Download for Windows
             </a>
             <a href="#" className="btn btn-ghost btn-lg hero-rise" style={{ animationDelay: "520ms" }}>
-              <svg className="i" viewBox="0 0 24 24"><path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"/></svg>
+              <svg className="i" viewBox="0 0 24 24"><path d="M12 .5C5.65.5.5 5.65.5 12c0 5.08 3.29 9.38 7.86 10.9.58.1.79-.25.79-.56v-2.17c-3.2.7-3.87-1.37-3.87-1.37-.52-1.33-1.27-1.68-1.27-1.68-1.04-.71.08-.7.08-.7 1.15.08 1.76 1.18 1.76 1.18 1.02 1.76 2.68 1.25 3.33.96.1-.74.4-1.25.72-1.54-2.55-.29-5.24-1.28-5.24-5.69 0-1.26.45-2.29 1.18-3.1-.12-.3-.51-1.47.11-3.07 0 0 .96-.31 3.16 1.18a11 11 0 016.16 0c2.2-1.49 3.16-1.18 3.16-1.18.62 1.6.23 2.78.11 3.07.74.81 1.18 1.84 1.18 3.1 0 4.42-2.7 5.39-5.26 5.68.41.36.78 1.06.78 2.13v3.15c0 .31.21.67.8.56A11.5 11.5 0 0023.5 12C23.5 5.65 18.35.5 12 .5z"/></svg>
               Star on GitHub
             </a>
           </div>
